b012adef57979f87be80356d9071e6764b762882
Arnaud de Grandmaison <arnaud.adegm@gmail.com>
2020-07-10 23:41:49 +0200

    Reformating. NFC.

--- include/ratrac/ratrac.h
+++ include/ratrac/ratrac.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <cmath>
 #include <iostream>
 #include <string>
-#include <cmath>
 
 namespace ratrac {
 // Classes
@@ -84,8 +84,7 @@ protected:
 private:
   /** Tests if two coordinates are approximalty equal. The difference must be
    * less than EPSILON. Currently, EPSILON = 0.00001. */
-  static bool close_to_equal(const DataTy &a,
-                             const DataTy &b) {
+  static bool close_to_equal(const DataTy &a, const DataTy &b) {
     const DataTy EPSILON = 0.00001;
     return std::abs(a - b) < EPSILON;
   }
