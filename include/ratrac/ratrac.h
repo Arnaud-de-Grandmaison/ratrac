1152d8951f8505ab154eeead66adf3097d8fcda6
Arnaud de Grandmaison <arnaud.adegm@gmail.com>
2020-07-10 23:40:14 +0200

    Simplify !
     - Don't use a default parameter type,
     - Reduce the number of template parameters.

--- include/ratrac/ratrac.h
+++ include/ratrac/ratrac.h
@@ -14,11 +14,9 @@ public:
   void say(std::ostream &os, const std::string &msg) const;
 };
 
-/** Current supported types: double, float. (Integers are not supported because EPSILON.) */
-using RayTracerDataType = double;
-
-/** Parent class of RayTracerVector(x, y, z, 0) and RayTracerPoint(x, y, z, 1).*/
-template <class DataTy = RayTracerDataType> class RayTracerTuple {
+/** Parent class of RayTracerVector(x, y, z, 0) and RayTracerPoint(x, y, z,
+ * 1).*/
+template <class DataTy> class RayTracerTuple {
 public:
 
   /** As shown here, Type must not be error or 0. It is a Point if w == 1 and a Vector if w == 0. */
@@ -94,8 +92,7 @@ private:
 };
 
 /** A class representing a Vector3 or a Vector4 whre w = 0. */
-template <class DataTy = RayTracerDataType>
-class RayTracerVector : public RayTracerTuple<DataTy> {
+template <class DataTy> class RayTracerVector : public RayTracerTuple<DataTy> {
 public:
   /** RayTracerVector = {x, y, z, 0} */
   RayTracerVector(DataTy x, DataTy y, DataTy z)
@@ -119,8 +116,7 @@ public:
 };
 
 /** A class representing a Point or a Vector4 where w = 1. */
-template <class DataTy = RayTracerDataType>
-class RayTracerPoint : public RayTracerTuple<DataTy> {
+template <class DataTy> class RayTracerPoint : public RayTracerTuple<DataTy> {
 public:
   /** RayTracerPoint = {x, y, z, 1} */
   RayTracerPoint(DataTy x, DataTy y, DataTy z)
@@ -152,18 +148,16 @@ public:
 // However adding two points does not really make sense, actually, it will return the middle between both.
 /** Adding two Tuples/Points/Vector returns a Tuple (because the Tests of the
 book does not required any other kind of return Type).*/
-template <class DataTy = RayTracerDataType,
-          class Obj1Kind = RayTracerTuple<DataTy>,
-          class Obj2Kind = RayTracerTuple<DataTy>>
-RayTracerTuple<DataTy>  operator+(const Obj1Kind &lhs,
-                                  const Obj2Kind &rhs) {
+template <class DataTy>
+RayTracerTuple<DataTy> operator+(const RayTracerTuple<DataTy> &lhs,
+                                 const RayTracerTuple<DataTy> &rhs) {
   RayTracerTuple<DataTy> tmp = lhs;
   tmp += rhs;
   return tmp;
 }
 
 /** Subtracting two Points returns a Vector. */
-template <class DataTy = RayTracerDataType>
+template <class DataTy>
 RayTracerVector<DataTy> operator-(const RayTracerPoint<DataTy> &lhs,
                                   const RayTracerPoint<DataTy> &rhs) {
   return RayTracerVector<DataTy>(lhs.x() - rhs.x(), lhs.y() - rhs.y(),
@@ -171,7 +165,7 @@ RayTracerVector<DataTy> operator-(const RayTracerPoint<DataTy> &lhs,
 }
 
 /** Subtracting a Vector from a Point returns a Point. */
-template <class DataTy = RayTracerDataType>
+template <class DataTy>
 RayTracerPoint<DataTy> operator-(const RayTracerPoint<DataTy> &lhs,
                                  const RayTracerVector<DataTy> &rhs) {
   RayTracerPoint<DataTy> tmp = lhs;
@@ -184,15 +178,19 @@ RayTracerPoint<DataTy> operator-(const RayTracerPoint<DataTy> &lhs,
 /** Substracting two Tuples/Vectors returns respectively a Tuple or a Vector./n
 WARNING: May makes trouble with RayTracerVector<DataTy>
 operator-(const RayTracerPoint<DataTy> &lhs, const RayTracerPoint<DataTy> &rhs)*/
-template <class DataTy = RayTracerDataType, class ObjKind = RayTracerTuple<DataTy>>
-ObjKind operator-(const ObjKind &lhs,
-                  const ObjKind &rhs) {
-  ObjKind tmp = lhs;
+template <class DataTy>
+RayTracerTuple<DataTy> operator-(const RayTracerTuple<DataTy> &lhs,
+                  const RayTracerTuple<DataTy> &rhs) {
+  RayTracerTuple<DataTy> tmp = lhs;
   tmp -= rhs;
   return tmp;
 }
 
 // Renaming classes for external usage
+
+/** Current supported types: double, float.
+ * Note: Integers are not supported because of EPSILON. */
+using RayTracerDataType = double;
 using Point = RayTracerPoint<RayTracerDataType>;
 using Vector = RayTracerVector<RayTracerDataType>;
 using Tuple = RayTracerTuple<RayTracerDataType>;
