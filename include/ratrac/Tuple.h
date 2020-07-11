6886069662aa83e6aa322e69d02a769ec2ae57a4
Arnaud de Grandmaison <arnaud.adegm@gmail.com>
2020-07-11 15:19:19 +0200

    Functional split of the files. NFC.

---
+++ include/ratrac/Tuple.h
@@ -0,0 +1,159 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+
+namespace ratrac {
+// Classes
+// =======
+
+/** RayTraceTuple represent the Tuples ratrac uses to manipulate point and vectors. */
+template <class DataTy> class RayTracerTuple {
+public:
+
+  /** As shown here, Type must not be error or 0. It is a Point if w == 1 and a Vector if w == 0. */
+  enum class Type {
+    error = 0,
+    point = 1,
+    vector = 2,
+  };
+
+  /** Initialise the Tuple/Vector4.*/
+  RayTracerTuple(DataTy x, DataTy y, DataTy z, DataTy w)
+      : m_tuple{x, y, z, w} {}
+
+  // Accessors
+  // =========
+
+  const DataTy &x() const { return m_tuple[0]; }
+  const DataTy &y() const { return m_tuple[1]; }
+  const DataTy &z() const { return m_tuple[2]; }
+  const DataTy &w() const { return m_tuple[3]; }
+  Type type() const {
+    if (m_tuple[3] == 1.0) {
+      return Type::point;
+    } else if (m_tuple[3] == 0.0) {
+      return Type::vector;
+    } else {
+      return Type::error;
+    }
+  }
+
+  // Operators
+  // =========
+
+  // equal/not equal
+
+  bool operator==(const RayTracerTuple<DataTy> &rhs) const {
+    return close_to_equal(x(), rhs.x()) && close_to_equal(y(), rhs.y()) &&
+           close_to_equal(z(), rhs.z()) && close_to_equal(w(), rhs.w());
+  }
+  bool operator!=(const RayTracerTuple<DataTy> &rhs) const {
+    return !operator==(rhs);
+  }
+
+  // operations
+
+  RayTracerTuple<DataTy> &operator+=(const RayTracerTuple<DataTy> &rhs) {
+    for (unsigned i=0; i<m_tuple.size(); i++)
+      m_tuple[i] += rhs.m_tuple[i];
+    return *this;
+  }
+  RayTracerTuple<DataTy> &operator-=(const RayTracerTuple<DataTy> &rhs) {
+    for (unsigned i=0; i<m_tuple.size(); i++)
+      m_tuple[i] -= rhs.m_tuple[i];
+    return *this;
+  }
+  RayTracerTuple<DataTy> &operator*=(const DataTy &rhs) {
+    for (DataTy &c: m_tuple)
+      c *= rhs;
+    return *this;
+  }
+
+
+  RayTracerTuple<DataTy> operator-() const {
+    return RayTracerTuple<DataTy>(-m_tuple[0], -m_tuple[1], -m_tuple[2],
+                                  -m_tuple[3]);
+  }
+
+private:
+  /** An array formated as following: Tuple(x, y, z, w). Args type:
+   * float/double. */
+  std::array<DataTy, 4> m_tuple;
+
+  /** Tests if two coordinates are approximalty equal. The difference must be
+   * less than EPSILON. Currently, EPSILON = 0.00001. */
+  static bool close_to_equal(const DataTy &a, const DataTy &b) {
+    const DataTy EPSILON = 0.00001;
+    return std::abs(a - b) < EPSILON;
+  }
+};
+
+/** Helper to create a Vectore, i.e. a Tuple with w = 0.0 */
+template <class DataTy>
+RayTracerTuple<DataTy> Vector(const DataTy &x, const DataTy &y,
+                              const DataTy &z) {
+  return RayTracerTuple<DataTy>(x, y, z, 0.0);
+}
+
+/** Helper to create a Point, i.e. a Tuple with w = 1.0 */
+template <class DataTy>
+RayTracerTuple<DataTy> Point(const DataTy &x, const DataTy &y,
+                             const DataTy &z) {
+  return RayTracerTuple<DataTy>(x, y, z, 1.0);
+}
+
+// Other/External operators
+// ========================
+
+// Adding two Tuples/Points/Vector returns repectively a Tuple, a Point or a
+// Vector.
+// However adding two points does not really make sense, actually, it will
+// return the middle between both.
+/** Adding two Tuples/Points/Vector returns a Tuple (because the Tests of the
+book does not required any other kind of return Type).*/
+template <class DataTy>
+RayTracerTuple<DataTy> operator+(const RayTracerTuple<DataTy> &lhs,
+                                 const RayTracerTuple<DataTy> &rhs) {
+  RayTracerTuple<DataTy> tmp = lhs;
+  tmp += rhs;
+  return tmp;
+}
+
+/** Substracting two Tuples/Vectors returns respectively a Tuple or a Vector.
+ * Subtracting two Points returns a Vector.
+ * Subtracting a Vector from a Point returns a Point. */
+template <class DataTy>
+RayTracerTuple<DataTy> operator-(const RayTracerTuple<DataTy> &lhs,
+                                 const RayTracerTuple<DataTy> &rhs) {
+  RayTracerTuple<DataTy> tmp = lhs;
+  tmp -= rhs;
+  return tmp;
+}
+
+/** Scalar multiplication. */
+template <class DataTy>
+RayTracerTuple<DataTy> operator*(const RayTracerTuple<DataTy> &lhs,
+                                 const DataTy &rhs) {
+  RayTracerTuple<DataTy> tmp = lhs;
+  tmp *= rhs;
+  return tmp;
+}
+template <class DataTy>
+RayTracerTuple<DataTy> operator*(const DataTy &lhs,
+                                 const RayTracerTuple<DataTy> &rhs) {
+  RayTracerTuple<DataTy> tmp = rhs;
+  tmp *= lhs;
+  return tmp;
+}
+
+// Renaming classes for external usage
+
+/** Current supported types: double, float.
+ * Note: Integers are not supported because of EPSILON. */
+using RayTracerDataType = double;
+//using Point = RayTracerPoint<RayTracerDataType>;
+//using Vector = RayTracerVector<RayTracerDataType>;
+using Tuple = RayTracerTuple<RayTracerDataType>;
+
+} // namespace ratrac
---
+++ test/test-ratrac.cpp
@@ -0,0 +1,138 @@
+#include "ratrac/Tuple.h"
+
+#include "gtest/gtest.h"
+
+using namespace ratrac;
+using namespace testing;
+
+TEST(RayTracerTuple, init) {
+  // Tuple initialisation
+  Tuple rtt(4.3, -4.2, 3.1, 1.0);
+  EXPECT_EQ(rtt.x(), 4.3);
+  EXPECT_EQ(rtt.y(), -4.2);
+  EXPECT_EQ(rtt.z(), 3.1);
+  EXPECT_EQ(rtt.w(), 1.0);
+  EXPECT_EQ(rtt.type(), Tuple::Type::point);
+  EXPECT_NE(rtt.type(), Tuple::Type::vector);
+
+  rtt = Tuple(4.3, -4.2, 3.1, 0.0);
+  EXPECT_EQ(rtt.x(), 4.3);
+  EXPECT_EQ(rtt.y(), -4.2);
+  EXPECT_EQ(rtt.z(), 3.1);
+  EXPECT_EQ(rtt.w(), 0.0);
+  EXPECT_EQ(rtt.type(), Tuple::Type::vector);
+  EXPECT_NE(rtt.type(), Tuple::Type::point);
+
+  // Point initialisation
+  Tuple p = Point(4.0, -4.0, 3.0);
+  EXPECT_TRUE(p == Tuple(4.0, -4.0, 3.0, 1.0));
+  EXPECT_FALSE(p != Tuple(4.0, -4.0, 3.0, 1.0));
+
+  // Vector initialisation
+  Tuple v = Vector(4.0, -4.0, 3.0);
+  EXPECT_TRUE(v == Tuple(4.0, -4.0, 3.0, 0.0));
+  EXPECT_FALSE(v != Tuple(4.0, -4.0, 3.0, 0.0));
+}
+
+TEST(RayTracerTuple, operations) {
+  // Sum
+  // ===
+
+  // Adding two Tuples
+  Tuple t1(3.0, 2.0, 1.0, 1.0);
+  Tuple t2(5.0, 6.0, 7.0, 1.0);
+  EXPECT_EQ(t1 + t2, Tuple(8.0, 8.0, 8.0, 2.0));
+  t1 += t2;
+  EXPECT_EQ(t1, Tuple(8.0, 8.0, 8.0, 2.0));
+
+  // Adding two Points
+  Tuple p1 = Point(3.0, 2.0, 1.0);
+  Tuple p2 = Point(5.0, 6.0, 7.0);
+  EXPECT_EQ(p1 + p2, Tuple(8.0, 8.0, 8.0, 2.0));
+
+  // Adding two Vectors
+  Tuple v1 = Vector(3.0, 2.0, 1.0);
+  Tuple v2 = Vector(5.0, 6.0, 7.0);
+  EXPECT_EQ(v1 + v2, Vector(8.0, 8.0, 8.0));
+
+  // Sum of a Point and a Vector => Point
+  Tuple p = Point(3.0, -2.0, 5.0);
+  Tuple v = Vector(-2.0, 3.0, 1.0);
+  EXPECT_EQ(p + v, Tuple(1.0, 1.0, 6.0, 1.0));
+  p += v;
+  EXPECT_EQ(p, Tuple(1.0, 1.0, 6.0, 1.0));
+
+  // Substraction
+  // ============
+
+  // Subtracting two Tuples
+  t1 = Tuple(3.0, 2.0, 1.0, 0.0);
+  t2 = Tuple(5.0, 6.0, 7.0, 1.0);
+  EXPECT_EQ(t1 - t2, Tuple(-2.0, -4.0, -6.0, -1.0));
+  t1 -= t2;
+  EXPECT_EQ(t1, Tuple(-2.0, -4.0, -6.0, -1.0));
+
+  // Subtracting two Points
+  p1 = Point(3.0, 2.0, 1.0);
+  p2 = Point(5.0, 6.0, 7.0);
+  EXPECT_EQ(p1 - p2, Vector(-2.0, -4.0, -6.0));
+
+  // Subtracting two Vectors
+  v1 = Vector(3.0, 2.0, 1.0);
+  v2 = Vector(5.0, 6.0, 7.0);
+  EXPECT_EQ(v1 - v2, Vector(-2.0, -4.0, -6.0));
+
+  // Subtracting a Vector from a Point
+  p = Point(3.0, 2.0, 1.0);
+  v = Vector(5.0, 6.0, 7.0);
+  EXPECT_EQ(p - v, Point(-2.0, -4.0, -6.0));
+  p -= v;
+  EXPECT_EQ(p, Point(-2.0, -4.0, -6.0));
+
+  // Minus operator/Negating a tuple
+
+  Tuple t(1.0, -2.0, 3.0, -4.0);
+  EXPECT_EQ(-t, Tuple(-1.0, 2.0, -3.0, 4.0));
+
+  // Not required by the book
+  // v = Vector(1.0, -2.0, 3.0);
+  // EXPECT_EQ(-t, Vector(1.0, 2.0, -3.0));
+
+  // Multiplication
+  // ==============
+
+  // Multiplying a tuple by a scalar (3.5)
+  t = Tuple(1.0, -2.0, 3.0, -4.0);
+  EXPECT_EQ(t * 3.5, Tuple(3.5, -7, 10.5, -14));
+  EXPECT_EQ(3.5 * t, Tuple(3.5, -7, 10.5, -14));
+
+  // Multiplying a tuple by a fraction (0.5)
+  EXPECT_EQ(t * 0.5, Tuple(0.5, -1, 1.5, -2));
+  EXPECT_EQ(0.5 * t, Tuple(0.5, -1, 1.5, -2));
+
+  // Divison
+  // =======
+
+  // Dividing a tuple by a scalar (2)
+
+  // Getting magnitude
+  // =================
+
+  // Simple tests
+
+  // Advanced tests
+
+  // Normalizing
+  // ===========
+
+  // Dot product (/scalar product)
+  // =============================
+
+  // Cross product
+  // =============
+}
+
+int main(int argc, char **argv) {
+  InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
