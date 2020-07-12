96660e5bd2c15ca3cad49a01e84ea465d89ded40
Arnaud de Grandmaison <arnaud.adegm@gmail.com>
2020-07-12 18:49:14 +0200

    Refactor: kill the no longer needed World class.
    And rename the main application to tick. The for now empty libratrac is
    kept on purpose, as it may have to contain stuff in the future, and it's
    a convenient way to make all executable using the library get the headers.

--- lib/ratrac/ratrac.cpp
+++ lib/ratrac/ratrac.cpp
@@ -1,9 +1,6 @@
 #include "ratrac/ratrac.h"
 
-#include <iostream>
-#include <string>
 
 using namespace ratrac;
 using namespace std;
 
-void World::say(ostream &os, const string &msg) const { os << msg << "\n"; }
--- main-tick.cpp
+++ main-tick.cpp
@@ -6,12 +6,45 @@
 using namespace ratrac;
 using namespace std;
 
+class Projectile {
+public:
+  Projectile(Tuple position, Tuple velocity)
+      : m_position{position}, m_velocity{velocity} {};
+
+  // Accessors
+  const Tuple &position() const { return m_position; }
+  const Tuple &velocity() const { return m_velocity; }
+
+private:
+  // a Point
+  Tuple m_position;
+  // a Vector
+  Tuple m_velocity;
+};
+
+class Environment {
+public:
+  Environment(Tuple gravity, Tuple wind) : m_gravity{gravity}, m_wind{wind} {};
+
+  // Accessors
+  const Tuple &gravity() const { return m_gravity; }
+  const Tuple &wind() const { return m_wind; }
+
+private:
+  // a Vector
+  Tuple m_gravity;
+  // a Vector
+  Tuple m_wind;
+};
+
+inline Projectile tick(Environment e, Projectile p) {
+  Tuple position = p.position() + p.velocity();
+  Tuple velocity = p.velocity() + e.gravity() + e.wind();
+  return Projectile(position, velocity);
+}
+
 int main(int argc, char *argv[]) {
   ios state(nullptr);
-
-  World w;
-
-  w.say(cout, "Hello World!");
 
   Projectile p(Point(0.0, 1.0, 0.0), Vector(1.0, 1.0, 0.0).normalize());
   Environment e(Vector(0.0, -0.1, 0.0), Vector(-0.01, 0.0, 0.0));
