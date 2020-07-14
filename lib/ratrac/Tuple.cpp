7135e00cf7331c0eebcb9cb9d300f1014c8adaf0
TheLichIsBack <liche.tuto@gmail.com>
2020-07-14 19:23:52 +0200

    Add << operator to Tuple

---
+++ lib/ratrac/Tuple.cpp
@@ -0,0 +1,7 @@
+#include "ratrac/Tuple.h"
+
+std::ostream &operator<<(std::ostream &os, const ratrac::Tuple &T) {
+  os << "Tuple { " << T.x() << ", " << T.y() << ", " << T.z() << ", " << T.w()
+     << "};\n";
+  return os;
+}
