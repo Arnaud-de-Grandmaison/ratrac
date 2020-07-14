4f97f1cd4693afb7fc43528a953cb227941cf179
TheLichIsBack <liche.tuto@gmail.com>
2020-07-14 19:26:54 +0200

    Add several operators, identity_matrice generator and transpose function and Separate tests.
    Add:
    - == operator
    - != operator
    - *= operator
    - * operator Matrice * Matrice, Matrice * Tuple

--- include/ratrac/Matrice.h
+++ include/ratrac/Matrice.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "ratrac/Tuple.h"
+
+#include <iostream>
 #include <ostream>
 #include <tuple>
 #include <vector>
@@ -24,10 +27,86 @@ public:
     return m_matrice[line][column];
   };
 
+  // Operators
+
+  bool operator==(const Matrice &rhs) const {
+    return m_matrice == rhs.m_matrice;
+  }
+  bool operator!=(const Matrice &rhs) const { return !operator==(rhs); }
+
+  // Both must be 4*4 matrices.
+  // #help: do we have to raise an error if matrices are not of size 4x4
+  //        Actually, it works for every case, except if both matrices have
+  //        a diferent size.
+  Matrice &operator*=(const Matrice &rhs) {
+    // Copy the current matrice to get the exact same size.
+    std::vector<std::vector<float>> future_matrix = m_matrice;
+    std::vector<float> added_rhs;
+    for (unsigned line(0); line < std::get<0>(rhs.size()); line++) {
+      for (unsigned column(0); column < std::get<1>(rhs.size()); column++) {
+        // Make sure the value is 0
+        future_matrix[line][column] = 0;
+        // Extract column
+        added_rhs.clear();
+        for (int it_line(0); it_line < rhs.m_matrice.size(); it_line++) {
+          added_rhs.push_back(rhs.m_matrice[it_line][column]);
+        }
+        // Do the specific operation
+        for (int it(0); it < m_matrice.size(); it++) {
+          future_matrix[line][column] += m_matrice[line][it] * added_rhs[it];
+        }
+      }
+    }
+    m_matrice = future_matrix;
+    return *this;
+  }
+
 private:
   // #Help: Should it be an array as a matrice is of fixed size.
   std::vector<std::vector<float>> m_matrice;
 };
+
+inline Matrice operator*(const Matrice &lhs, const Matrice &rhs) {
+  Matrice tmp = lhs;
+  tmp *= rhs;
+  return tmp;
+}
+
+inline Tuple operator*(const Matrice &lhs, const Tuple &rhs) {
+  std::vector<float> future_tuple;
+  for (unsigned line(0); line < std::get<0>(lhs.size()); line++) {
+    future_tuple.push_back(0);
+    future_tuple[line] += lhs.at(line, 0) * rhs.x();
+    future_tuple[line] += lhs.at(line, 1) * rhs.y();
+    future_tuple[line] += lhs.at(line, 2) * rhs.z();
+    future_tuple[line] += lhs.at(line, 3) * rhs.w();
+  }
+  return Tuple(future_tuple[0], future_tuple[1], future_tuple[2],
+               future_tuple[3]);
+}
+
+/** Return an identity matrice being as following:
+Matrice {    1.0,    0.0,    0.0,    0.0},
+        {    0.0,    1.0,    0.0,    0.0},
+        {    0.0,    0.0,    1.0,    0.0},
+        {    0.0,    0.0,    0.0,    1.0}}*/
+inline Matrice identity_matrix() {
+  return Matrice(std::vector<std::vector<float>>(
+      {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}));
+}
+
+inline Matrice transpose(const Matrice &M) {
+  std::vector<std::vector<float>> future_matrice;
+
+  // Extract column; M_column = future_line
+  for (unsigned M_column(0); M_column < std::get<1>(M.size()); M_column++) {
+    future_matrice.push_back(std::vector<float>());
+    for (int it_line(0); it_line < std::get<0>(M.size()); it_line++) {
+      future_matrice[M_column].push_back(M.matrice()[it_line][M_column]);
+    }
+  }
+  return Matrice(future_matrice);
+}
 
 } // namespace ratrac
 
--- test/test-Matrice.cpp
+++ test/test-Matrice.cpp
@@ -1,4 +1,5 @@
 #include "ratrac/Matrice.h"
+#include "ratrac/Tuple.h"
 
 #include "gtest/gtest.h"
 
@@ -10,6 +11,49 @@ using namespace ratrac;
 using namespace testing;
 
 TEST(RayTracerMatrice, init) {
+  // init Tests
+  // ==========
+
+  // 4*4 matrice
+
+  std::vector<std::vector<float>> matrice = {{1, 2, 3, 4},
+                                             {5.5, 6.5, 7.5, 8.5},
+                                             {9, 10, 11, 12},
+                                             {13.5, 14.5, 15.5, 16.5}};
+  Matrice M = matrice;
+  EXPECT_EQ(M.at(0, 0), 1);
+  EXPECT_EQ(M.at(0, 3), 4);
+  EXPECT_EQ(M.at(1, 0), 5.5);
+  EXPECT_EQ(M.at(1, 2), 7.5);
+  EXPECT_EQ(M.at(2, 2), 11);
+  EXPECT_EQ(M.at(3, 0), 13.5);
+  EXPECT_EQ(M.at(3, 2), 15.5);
+  std::tuple<int, int> t(4, 4);
+  EXPECT_EQ(M.size(), t);
+
+  // 3*3 Matrice
+
+  matrice = {{-3, 5, 0}, {1, -2, -7}, {0, 1, 1}};
+  M = matrice;
+  EXPECT_EQ(M.at(0, 0), -3);
+  EXPECT_EQ(M.at(1, 1), -2);
+  EXPECT_EQ(M.at(2, 2), 1);
+  t = std::tuple<int, int>(3, 3);
+  EXPECT_EQ(M.size(), t);
+
+  // 2*2 Matrice
+
+  matrice = {{-3, 5}, {1, -2}};
+  M = matrice;
+  EXPECT_EQ(M.at(0, 0), -3);
+  EXPECT_EQ(M.at(0, 1), 5);
+  EXPECT_EQ(M.at(1, 0), 1);
+  EXPECT_EQ(M.at(1, 1), -2);
+  t = std::tuple<int, int>(2, 2);
+  EXPECT_EQ(M.size(), t);
+}
+
+TEST(RayTracerMatrice, operators) {
   // << operator Test
   std::vector<std::vector<float>> matrice = {{1.0f, 1.0f, 1.0f, 1.0f},
                                              {1.0f, 1.0f, 1.0f, 1.0f},
@@ -23,45 +67,57 @@ TEST(RayTracerMatrice, init) {
                                  "\t{    1.0,    1.0,    1.0,    1.0},\n"
                                  "\t{    1.0,    1.0,    1.0,    1.0}}\n");
 
-  // init Tests
-  // ==========
+  // Equity test
 
-  // 4*4 matrice
+  std::vector<std::vector<float>> matrice1 = {
+      {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 8, 7, 6}, {5, 4, 3, 2}};
+  Matrice M1 = matrice1;
+  std::vector<std::vector<float>> matrice2 = {
+      {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 8, 7, 6}, {5, 4, 3, 2}};
+  Matrice M2 = matrice2;
+  EXPECT_EQ(M1, M2);
+  matrice2 = {{2, 3, 4, 5}, {6, 7, 8, 9}, {8, 7, 6, 5}, {4, 3, 2, 1}};
+  M2 = matrice2;
+  EXPECT_NE(M1, M2);
 
-  matrice = {{1, 2, 3, 4},
-             {5.5, 6.5, 7.5, 8.5},
-             {9, 10, 11, 12},
-             {13.5, 14.5, 15.5, 16.5}};
-  M = matrice;
-  EXPECT_EQ(M.at(0, 0), 1);
-  EXPECT_EQ(M.at(0, 3), 4);
-  EXPECT_EQ(M.at(1, 0), 5.5);
-  EXPECT_EQ(M.at(1, 2), 7.5);
-  EXPECT_EQ(M.at(2, 2), 11);
-  EXPECT_EQ(M.at(3, 0), 13.5);
-  EXPECT_EQ(M.at(3, 2), 15.5);
-  std::tuple<int, int> t(4, 4);
-  EXPECT_EQ(M.size(), t);
+  // Mutliply test
 
-  // 3*3 Matrice
+  // Multiply Matrices
 
-  matrice = {{1, 2, 3}, {5.5, 6.5, 7.5}, {9, 10, 11}};
-  M = matrice;
-  EXPECT_EQ(M.at(0, 0), 1);
-  EXPECT_EQ(M.at(1, 0), 5.5);
-  EXPECT_EQ(M.at(1, 2), 7.5);
-  EXPECT_EQ(M.at(2, 2), 11);
-  t = std::tuple<int, int>(3, 3);
-  EXPECT_EQ(M.size(), t);
+  matrice1 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 8, 7, 6}, {5, 4, 3, 2}};
+  M1 = matrice1;
+  matrice2 = {{-2, 1, 2, 3}, {3, 2, 1, -1}, {4, 3, 6, 5}, {1, 2, 7, 8}};
+  M2 = matrice2;
+  matrice = {{20, 22, 50, 48},
+             {44, 54, 114, 108},
+             {40, 58, 110, 102},
+             {16, 26, 46, 42}};
+  M1 *= M2;
+  EXPECT_EQ(M1, Matrice(matrice));
+  M1 = matrice1;
+  EXPECT_EQ(M1 * M2, Matrice(matrice));
 
-  // 2*2 Matrice
+  // Multiply a Matrice by a Tuple
 
-  matrice = {{1, 2}, {5.5, 6.5}};
-  M = matrice;
-  EXPECT_EQ(M.at(0, 0), 1);
-  EXPECT_EQ(M.at(1, 0), 5.5);
-  t = std::tuple<int, int>(2, 2);
-  EXPECT_EQ(M.size(), t);
+  matrice1 = {{1, 2, 3, 4}, {2, 4, 4, 2}, {8, 6, 4, 1}, {0, 0, 0, 1}};
+  M = matrice1;
+  Tuple b(1, 2, 3, 1);
+  EXPECT_EQ(M * b, Tuple(18, 24, 33, 1));
+}
+
+TEST(RayTracerMatrice, other_stuff) {
+  // Identity matrice tests
+
+  Matrice M(std::vector<std::vector<float>>(
+      {{0, 1, 2, 4}, {1, 2, 4, 8}, {2, 4, 8, 16}, {4, 8, 16, 32}}));
+  EXPECT_EQ(M * identity_matrix(), M);
+  Tuple a(1., 2., 3., 4.);
+  EXPECT_EQ(identity_matrix() * a, a);
+
+  M = std::vector<std::vector<float>>(
+      {{0, 9, 3, 0}, {9, 8, 0, 8}, {1, 8, 5, 3}, {0, 0, 5, 8}});
+  EXPECT_EQ(transpose(M),
+            Matrice({{0, 9, 1, 0}, {9, 8, 8, 0}, {3, 0, 5, 5}, {0, 8, 3, 8}}));
 }
 
 int main(int argc, char **argv) {
