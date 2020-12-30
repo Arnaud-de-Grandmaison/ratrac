dc3024a5f5d96029ff933abf40e34ac7df9d6a3a
Arnaud de Grandmaison <arnaud.adegm@gmail.com>
2020-12-30 23:33:03 +0100

    [Color] Typo fix in output operator.

--- lib/ratrac/Color.cpp
+++ lib/ratrac/Color.cpp
@@ -1,7 +1,7 @@
 #include "ratrac/Color.h"
 
 std::ostream &operator<<(std::ostream &os, const ratrac::Color &C) {
-  os << "Color { reg:" << C.red() << ", green:" << C.green()
+  os << "Color { red:" << C.red() << ", green:" << C.green()
      << ", blue:" << C.blue() << ", alpha:" << C.alpha() << "}";
   return os;
 }
