cbdcc10ecf80bd1097471811be5356ff13d8e626
Arnaud de Grandmaison <arnaud.adegm@gmail.com>
2021-04-23 18:08:01 +0200

    [Color] Get rid of template.

--- include/ratrac/Color.h
+++ include/ratrac/Color.h
@@ -12,87 +12,85 @@ namespace ratrac {
 
 /** Color represent a Color which is used by ratrac to manipulate
  * Points and Vectors. */
-template <class ColorTy> class RayTracerColor {
-  static_assert(std::is_floating_point<ColorTy>::value,
+class Color {
+  static_assert(std::is_floating_point<ratrac::RayTracerColorType>::value,
                 "Color ColorTy must be a floating point type.");
 
 public:
-  /** Initialise the Color/Tuple/Vector4.*/
-  RayTracerColor(ColorTy red, ColorTy green, ColorTy blue)
-      : m_color{red, green, blue, 1.0f} {}
-  RayTracerColor(ColorTy red, ColorTy green, ColorTy blue, ColorTy alpha)
-      : m_color{red, green, blue, alpha} {}
-  RayTracerColor() : m_color{0.0f, 0.0f, 0.0f, 1.0f} {}
+  typedef ratrac::RayTracerColorType ColorType;
 
-  typedef ColorTy ColorType;
+  /** Initialise the Color/Tuple/Vector4.*/
+  Color(ColorType red, ColorType green, ColorType blue)
+      : m_color{red, green, blue, 1.0f} {}
+  Color(ColorType red, ColorType green, ColorType blue, ColorType alpha)
+      : m_color{red, green, blue, alpha} {}
+  Color() : m_color{0.0f, 0.0f, 0.0f, 1.0f} {}
 
   // Accessors
   // =========
 
-  ColorTy &red() { return m_color[0]; }
-  ColorTy &green() { return m_color[1]; }
-  ColorTy &blue() { return m_color[2]; }
-  ColorTy &alpha() { return m_color[3]; }
+  ColorType &red() { return m_color[0]; }
+  ColorType &green() { return m_color[1]; }
+  ColorType &blue() { return m_color[2]; }
+  ColorType &alpha() { return m_color[3]; }
 
-  const ColorTy &red() const { return m_color[0]; }
-  const ColorTy &green() const { return m_color[1]; }
-  const ColorTy &blue() const { return m_color[2]; }
-  const ColorTy &alpha() const { return m_color[3]; }
+  const ColorType &red() const { return m_color[0]; }
+  const ColorType &green() const { return m_color[1]; }
+  const ColorType &blue() const { return m_color[2]; }
+  const ColorType &alpha() const { return m_color[3]; }
 
   // Operators
   // =========
 
   // equal/not equal
 
-  bool operator==(const RayTracerColor &rhs) const {
+  bool operator==(const Color &rhs) const {
     return close_to_equal(red(), rhs.red()) &&
            close_to_equal(green(), rhs.green()) &&
            close_to_equal(blue(), rhs.blue()) &&
            close_to_equal(alpha(), rhs.alpha());
   }
-  bool operator!=(const RayTracerColor &rhs) const { return !operator==(rhs); }
+  bool operator!=(const Color &rhs) const { return !operator==(rhs); }
 
   // operations
 
-  RayTracerColor &operator+=(const RayTracerColor &rhs) {
+  Color &operator+=(const Color &rhs) {
     for (unsigned i = 0; i < m_color.size() - 1; i++)
       m_color[i] += rhs.m_color[i];
     return *this;
   }
-  RayTracerColor &operator-=(const RayTracerColor &rhs) {
+  Color &operator-=(const Color &rhs) {
     for (unsigned i = 0; i < m_color.size() - 1; i++)
       m_color[i] -= rhs.m_color[i];
     return *this;
   }
-  RayTracerColor &operator*=(RayTracerColorType rhs) {
+  Color &operator*=(ColorType rhs) {
     for (unsigned i = 0; i < m_color.size() - 1; i++)
       m_color[i] *= rhs;
     return *this;
   }
-  RayTracerColor &operator*=(const RayTracerColor &rhs) {
+  Color &operator*=(const Color &rhs) {
     for (unsigned i = 0; i < m_color.size(); i++)
       m_color[i] *= rhs.m_color[i];
     return *this;
   }
-  RayTracerColor &operator/=(RayTracerColorType rhs) {
+  Color &operator/=(ColorType rhs) {
     for (unsigned i = 0; i < m_color.size() - 1; i++)
       m_color[i] /= rhs;
     return *this;
   }
 
   // Color helpers.
-  static RayTracerColor BLACK() { return RayTracerColor(); }
-  static RayTracerColor WHITE() { return RayTracerColor(1, 1, 1); }
-  static RayTracerColor RED() { return RayTracerColor(1, 0, 0); }
-  static RayTracerColor GREEN() { return RayTracerColor(0, 1, 0); }
-  static RayTracerColor BLUE() { return RayTracerColor(0, 0, 1); }
+  static Color BLACK() { return Color(); }
+  static Color WHITE() { return Color(1, 1, 1); }
+  static Color RED() { return Color(1, 0, 0); }
+  static Color GREEN() { return Color(0, 1, 0); }
+  static Color BLUE() { return Color(0, 0, 1); }
 
 private:
   // An array formated as following: RayTracerColor(red, green, blue, alpha).
-  std::array<ColorTy, 4> m_color;
+  std::array<ColorType, 4> m_color;
 };
-
-using Color = RayTracerColor<RayTracerColorType>;
 
 // Other/External operators
 // ========================
