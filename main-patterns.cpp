aacdd01553cadedfebb0068a39537122209bd6d2
Arnaud de Grandmaison <arnaud.adegm@gmail.com>
2021-05-31 12:42:14 +0200

    [Pattern] Add an application to visualize patterns.

---
+++ main-patterns.cpp
@@ -0,0 +1,84 @@
+#include "ratrac/Camera.h"
+#include "ratrac/Canvas.h"
+#include "ratrac/Color.h"
+#include "ratrac/Light.h"
+#include "ratrac/Material.h"
+#include "ratrac/Patterns.h"
+#include "ratrac/Shapes.h"
+#include "ratrac/Tuple.h"
+#include "ratrac/World.h"
+
+#include <cmath>
+#include <fstream>
+
+using namespace ratrac;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  World world;
+
+  // The floor.
+  Plane *floor = new Plane();
+  floor->transform(Matrice::rotation_y(-M_PI / 2));
+  Material m;
+  m.pattern(Ring(Color(0.25, 0.25, 0.25), Color(0.8, 0.8, 0.8),
+                 Matrice::scaling(0.5, 0.5, 0.5)));
+  m.specular(0);
+  floor->material(m);
+  world.append(floor);
+
+  // The big sphere.
+  Sphere *middle = new Sphere();
+  middle->transform(Matrice::translation(-1.0, 1.0, 2.0) *
+                    Matrice::rotation_y(-M_PI / 2));
+  Material m2;
+  m2.pattern(Stripes(Color(0.1, 0.8, 0.1), Color(0.5, 0.8, 0.5),
+                     Matrice::scaling(0.15, 0.15, 0.15) *
+                         Matrice::rotation_z(-M_PI / 8) *
+                         Matrice::rotation_y(-M_PI / 3)));
+  m2.diffuse(0.7);
+  m2.specular(0.3);
+  middle->material(m2);
+  world.append(middle);
+
+  // The right sphere.
+  Sphere *right = new Sphere();
+  right->transform(Matrice::translation(1.5, 0.5, -0.5) *
+                   Matrice::scaling(0.5, 0.5, 0.5));
+  Material m3;
+  m3.pattern(Gradient(Color::RED(), Color(1, 1, 0)));
+  m3.diffuse(0.7);
+  m3.specular(0.3);
+  right->material(m3);
+  world.append(right);
+
+  // The left sphere.
+  Sphere *left = new Sphere();
+  left->transform(Matrice::translation(-1.5, 0.33, -0.75) *
+                  Matrice::scaling(0.33, 0.33, 0.33));
+  Material m4;
+  m4.pattern(Checkers(Color(0.05, 0.25, 0.05), Color(0.5, 0.8, 0.5),
+                      Matrice::scaling(0.1, 0.1, 0.1)));
+  m4.color(Color(1, 0.8, 0.1));
+  m4.diffuse(0.7);
+  m4.specular(0.3);
+  left->material(m4);
+  world.append(left);
+
+  // The light source is white, shining from above and to the left:
+  world.lights().push_back(LightPoint(Point(-10, 10, -10), Color(1, 1, 1)));
+
+  // Camera camera(100, 50, M_PI / 3.);
+  Camera camera(400, 200, M_PI / 3.);
+  camera.transform(
+      view_transform(Point(0, 1.5, -5), Point(0, 1, 0), Vector(0, 1, 0)));
+
+  // Render the world to a canvas.
+  Canvas C = camera.render(world);
+
+  // Save the scene.
+  ofstream file("patterns.ppm");
+  C.to_ppm(file);
+
+  return 0;
+}
